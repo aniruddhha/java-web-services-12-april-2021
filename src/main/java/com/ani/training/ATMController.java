d374031c2feda9ddb3c77f018e23d1aeb0ef6b5e
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-04-12 22:28:56 +0530

    initial commit

---
+++ src/main/java/com/ani/training/ATMController.java
@@ -0,0 +1,16 @@
+package com.ani.training;
+
+public class ATMController {    // class
+
+    public void withdraw() {    // function
+
+    }
+
+    public void checkBalance() {    // function
+
+    }
+
+    public void deposit() {     // function
+
+    }
+}
