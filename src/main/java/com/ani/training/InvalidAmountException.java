622437bc5b5177e74d4e457f729be3732cd90919
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-04-13 22:58:24 +0530

    exception handling - custom exception

--- src/main/java/com/ani/training/InvalidAmountException.java
+++ src/main/java/com/ani/training/InvalidAmountException.java
@@ -1,5 +1,12 @@
 package com.ani.training;
 
+/**
+ *  - What is exception ? -> unwanted situation
+ *  - How Spring boot handles exception by default ? -> by showing predefined JSON object
+ *  - How do you throw exception ? -> throw new YourExceptionClass
+ *  - How do you customize exception ? -> by extending to runtime exception class
+ *  - How do you handle exception in spring boot ? -> by writing method with @ExceptionHandler annotation
+ * */
 public class InvalidAmountException extends RuntimeException {
 
     @Override
