3ca7c7a46442d97667d13efed044febdde93164f
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-04-13 23:14:25 +0530

    exception handling - custom exception

--- src/main/java/com/ani/training/ATMController.java
+++ src/main/java/com/ani/training/ATMController.java
@@ -2,14 +2,18 @@ package com.ani.training;
 
 import org.springframework.web.bind.annotation.*;
 /*
-*  - What is Rest ?
-*  - What is Controller ?
-*  - What is Rest Controller ?
-*  - What is Get ?
-*  - What is Put ?
-*  - What is Post ?
-*  - What is Delete ?
-* */
+   - What is web service ? One software is calling method of another software, this mechanism is referred as web service.
+   - What is http ? -> Hypertext Transfer Protocol. Generally Web Services Communicate over the http protocol.
+   - What are data exchange formats ? -> Web Service transmits or receives data in predefined format, this format has to be accepted by clients for communication. HTML, XML , JSON etc etc are the mostly used data exchange formats.
+*  - What is Rest ? -> REST is a guideline. It says concepts like Resource (Anything that performs predefined action and which
+*    can be accessed by URL),Collection and URL (Uniform Resource Locator)
+*  - What is Controller ? -> Is main part of your application which communicates with user.
+*  - What is Rest Controller ? -> it is the controller which follows REST Guidelines.
+*  - What is Get ? -> getting resource from server
+*  - What is Put ? -> putting/updating resource on the server
+*  - What is Post ? -> saving resource on the server
+*  - What is Delete ? -> deleting resource from server
+* * */
 
 @RestController
 public class ATMController {    // class
