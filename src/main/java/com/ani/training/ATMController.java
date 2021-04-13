8004af36b22e512e28f56803e41753b7b8208ee1
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-04-13 21:48:01 +0530

    exception handling

--- src/main/java/com/ani/training/ATMController.java
+++ src/main/java/com/ani/training/ATMController.java
@@ -1,6 +1,15 @@
 package com.ani.training;
 
 import org.springframework.web.bind.annotation.*;
+/*
+*  - What is Rest ?
+*  - What is Controller ?
+*  - What is Rest Controller ?
+*  - What is Get ?
+*  - What is Put ?
+*  - What is Post ?
+*  - What is Delete ?
+* */
 
 @RestController
 public class ATMController {    // class
@@ -14,12 +23,18 @@ public class ATMController {    // class
 
     @PostMapping("/deposit/{amount}")
     public String deposit(@PathVariable Integer amount) {
+        if (amount <= 0) {
+            return  "Invalid Amount";
+        }
         amt = amt + amount;
         return  "Amount deposit successfully "+amount;
     }
 
     @DeleteMapping("/withdraw/{amount}")
     public String withdraw(@PathVariable Integer amount) {
+        if (amount <= 0) {
+            return  "Invalid Amount";
+        }
         amt = amt - amount;
         return  "Amount withdraw successfully "+amount;
     }
