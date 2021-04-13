8d68577f1be01c0fc52f4d16f7dfd9e2e9d12ac1
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-04-13 20:30:39 +0530

    @postmapping

--- src/main/java/com/ani/training/ATMController.java
+++ src/main/java/com/ani/training/ATMController.java
@@ -1,13 +1,23 @@
 package com.ani.training;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ATMController {    // class
 
+    private Integer amt = 0;
+
     @GetMapping("/balance")
     public Integer checkBalance() {    // function
-        return 1000;
+        return amt;
+    }
+
+    @PostMapping("/deposit/{amount}")
+    public String deposit(@PathVariable Integer amount) {
+        amt = amount;
+        return  "Amount deposit successfully";
     }
 }
