b6fa99d684c5ebf297702cc80d410e608efc9815
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-04-15 22:21:23 +0530

    @jpa - insert and select

--- src/main/java/com/ani/training/AcController.java
+++ src/main/java/com/ani/training/AcController.java
@@ -1,20 +1,40 @@
 package com.ani.training;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class AcController {
 
-    @PostMapping("/create")
+    @Autowired
+    private BankAccountDao dao;
+
+    @PostMapping("/create/basic")
     public String createNewAccount() {
-        /*
-        * - account holder name
-        * - account balance
-        * */
+        BankAccount ac = new BankAccount();
+        ac.setId(123);
+        ac.setAcNm("Ani");
+        ac.setBal(1234);
 
-        // saving data to database for creating account.
+        dao.save(ac);
 
-        return "";
+        return "Account Created Successfully";
+    }
+
+    @PostMapping("/create")
+    public String createNewAccountFromObject(@RequestBody BankAccount ac) {
+        dao.save(ac); // insert query
+        return "Account Created Successfully";
+    }
+
+    @GetMapping("/accounts")
+    public List<BankAccount> listAccount() {
+        List<BankAccount> accounts = dao.findAll();
+        return accounts;
     }
 }
