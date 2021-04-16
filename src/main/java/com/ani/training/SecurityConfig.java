c5bea285cf52bda78f777815945428a89351a1da
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-04-16 21:59:30 +0530

    security - spring security

---
+++ src/main/java/com/ani/training/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.ani.training;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder bld) throws Exception {
+        bld.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}pass123")
+                .roles("USER");
+    }
+}
