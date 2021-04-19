8e214688e7b5bcb462c9de82b1037ad8df06d05e
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-04-19 21:27:01 +0530

    rest consuming

--- rest-consuming/app.js
+++ rest-consuming/app.js
@@ -1,9 +1,3 @@
-function callWs() {
-    console.log('Clicked')
-
-    getData()
-}
-
 function getData() {
     const client = new XMLHttpRequest(); // http client
     client.onreadystatechange = function() {
@@ -12,6 +6,38 @@ function getData() {
       }
     };
     client.open("GET", "http://localhost:8080/balance", true); // method, url , async
-//    client.setRequestHeader("Authorization", "Basic " + btoa("admin:pass123")); // user and password in base64
+    client.send();
+}
+
+function postData() {
+    const client = new XMLHttpRequest(); // http client
+    client.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log('Server Response - '+this.responseText)
+      }
+    };
+    client.open("POST", "http://localhost:8080/deposit/65987", true); // method, url , async
+    client.send();
+}
+
+function deleteData() {
+    const client = new XMLHttpRequest(); // http client
+    client.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log('Server Response - '+this.responseText)
+      }
+    };
+    client.open("DELETE", "http://localhost:8080/withdraw/10", true); // method, url , async
+    client.send();
+}
+
+function putData() {
+    const client = new XMLHttpRequest(); // http client
+    client.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log('Server Response - '+this.responseText)
+      }
+    };
+    client.open("PUT", "http://localhost:8080/penalty", true); // method, url , async
     client.send();
 }
